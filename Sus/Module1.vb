03c4218eb26536ce9ee79d1b110d9cf2de9826fa
ArloRussell <arlorussell2008@gmail.com>
2024-02-09 08:13:20 -0500

    Add project files.

---
+++ Sus/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Do
+            Console.WriteLine("  ▄█▀▀▀█▄█       ")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine(" ▄██    ▀█                  ")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine(" ▀███▄   ▀███  ▀███  ▄██▀███")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine("   ▀█████▄ ██    ██  ██   ▀▀")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine(" ▄     ▀██ ██    ██  ▀█████▄")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine(" ██     ██ ██    ██  █▄   ██")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine(" █▀█████▀  ▀████▀███▄██████▀")
+            Threading.Thread.Sleep(100)
+            Console.WriteLine("")
+            Console.WriteLine("")
+            Console.WriteLine("")
+
+
+
+        Loop
+
+    End Sub
+
+End Module
